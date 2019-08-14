8342e4111d21c93531a38069c71f486c2fb60fdd
Olusegun Ekoh <segun.ekoh@gmail.com>
2019-08-15 00:29:30 +0100

    add App component :fire:

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-ReactDOM.render(
-  <ul>
-    <li>To Eat</li>
-    <li>To Play</li>
-    <li>To Do</li>
-  </ul>,
-  document.getElementById('root')
+
+const App = () => {
+  return (
+    <div>
+        <h1>To Do:</h1>
+        <ul className="myApp">
+           <li>Play</li>
+          <li>Sleep</li>
+          <li>Iron</li>
+       </ul>
+    </div> 
   );
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
 
