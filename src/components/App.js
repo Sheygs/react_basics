be46ab4007c12497f77c8d65e6c5630b34defeef
Olusegun Ekoh <segun.ekoh@gmail.com>
2019-08-15 01:10:21 +0100

    add component folder :smile:

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+const App = () => {
+  return (
+    <div className="container">
+       <h1 className="title">My name is Olusegun</h1>
+       <p>I am a self-taught developer who would build the next big thing!
+         My top 3 vacations are: <br/>
+       </p>
+       <ul>
+         <li>Bahamas</li>
+         <li>Toronto</li>
+         <li>New York</li>
+       </ul>
+    </div>
+  )
+};
+
+export default App;
