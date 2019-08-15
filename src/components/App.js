aca89f58175d586d89cf54dc5a9e2ef55c37c731
Olusegun Ekoh <segun.ekoh@gmail.com>
2019-08-15 21:29:14 +0100

    add component folder :fire:

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
+import Navbar from './Navbar';
+import MainContent from './MainContent';
+import Footer from './Footer';
 const App = () => {
   return (
-    <div className="container">
-       <h1 className="title">My name is Olusegun</h1>
-       <p>I am a self-taught developer who would build the next big thing!
-         My top 3 vacations are: <br/>
-       </p>
-       <ul>
-         <li>Bahamas</li>
-         <li>Toronto</li>
-         <li>New York</li>
-       </ul>
-    </div>
+    <div className ="App">
+        <Navbar/>
+        <MainContent/>
+        <Footer/>
+    </div>  
   )
 };
 
