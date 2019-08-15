3033e8165f5340c398f013a8fea485fb3ce2003b
Olusegun Ekoh <segun.ekoh@gmail.com>
2019-08-15 01:11:34 +0100

    some modifications

--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-const App = () => {
-  return (
-    <div className="container">
-       <h1 className="title">My name is Olusegun</h1>
-       <p>I am a self-taught developer who would build the next big thing!
-         My top 3 vacations are: <br/>
-       </p>
-       <ul>
-         <li>Bahamas</li>
-         <li>Toronto</li>
-         <li>New York</li>
-       </ul>
-    </div>
-  )
-};
-
+import App from './components/App';
 ReactDOM.render(<App/>, document.getElementById('root'));
 
