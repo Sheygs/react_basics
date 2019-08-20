6374a3093393f05625095256054faff340153924
Olusegun Ekoh <segun.ekoh@gmail.com>
2019-08-20 17:44:17 +0100

    :fire:

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,42 @@ const App = () => {
   return (
     <div className="contact">
       <ContactCard 
-           name="Mr Sorenson" 
-           imgUrl="https://placekitten.com/300/200"
-           phoneNumber="(555) 345-23"
-           email="sorenson@gmail.com"
+        contact={
+                  {
+                   name:"Mr Sorenson", 
+                   imgUrl:"https://placekitten.com/300/200",  
+                   phoneNumber:"(555) 345-23",  
+                   email:"sorenson@gmail.com"
+                  }
+              }
       />
       <ContactCard 
-           name="Mr Mark" 
-           imgUrl="https://placekitten.com/400/200"
-           phoneNumber="(555) 312-24"
-           email="mark@gmail.com"
+           contact =  {{
+                    name:"Mr Mark",
+                    imgUrl:"https://placekitten.com/400/200",
+                    phoneNumber:"(555) 312-24",
+                    email:"mark@gmail.com"
+             }  
+           }    
+      />
+      <ContactCard
+            contact =  {{
+              name:"Mr Jackson",
+              imgUrl:"https://placekitten.com/400/300",
+              phoneNumber:"(555) 459-25",
+              email:"jackson@gmail.com"
+       }  
+     }  
+    
       />
       <ContactCard 
-           name="Mr Jackson" 
-           imgUrl="https://placekitten.com/400/300"
-           phoneNumber="(555) 459-25"
-           email="jackson@gmail.com"
-      />
-      <ContactCard 
-           name="Mr Micheal" 
-           imgUrl="https://placekitten.com/200/100"
-           phoneNumber="(555) 543-26"
-           email="micheal@gmail.com"
+       contact =  {{
+        name:"Mr Micheal",
+        imgUrl:"https://placekitten.com/200/100",
+        phoneNumber:"(555) 543-26",
+        email:"micheal@gmail.com"
+        }  
+       } 
       />
     </div>
   )
--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -3,10 +3,10 @@ const ContactCard = props => {
   console.log(props);
   return (
      <div className="contact-card">
-       <img alt="cat" src={props.imgUrl}/>
-       <h3>Name: {props.name} </h3>
-       <p>Phone No: {props.phoneNumber}</p>
-       <p>Email : {props.email}</p>
+       <img alt="cat" src={props.contact.imgUrl}/>
+       <h3>Name: {props.contact.name} </h3>
+       <p>Phone No: {props.contact.phoneNumber}</p>
+       <p>Email : {props.contact.email}</p>
      </div>
   )
 };
