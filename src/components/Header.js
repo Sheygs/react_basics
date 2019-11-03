9b0c55edf5760c72780349713980e66bb38089c0
Olusegun Ekoh <segun.ekoh@gmail.com>
2019-11-03 15:07:51 +0100

    :fire:

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+     <ul>
+         <li>This is my Header</li>
+     </ul>
+  )
+}
+
+export default Header;
--- src/container/app.js
+++ src/container/app.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './app.css';
 //import Lists from '../components/Lists';
-import MyInfo from '../components/MyInfo';
+//import MyInfo from '../components/MyInfo';
+import Header from '../components/Header';
+import MainContent from '../components/MainContent';
+import Footer from '../components/Footer';
 const App = _ => {
   return (
     <div>
-       <MyInfo/>
+        <Header/>
+        <MainContent/>
+        <Footer/>
     </div>
   )
 }  
