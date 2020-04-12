4c6416d4067c133ac7ca455c3eb12b2e14e12449
sheygs13 <segun.ekoh@gmail.com>
2020-04-12 16:34:25 +0100

    add todoData to persist

---
+++ src/components/todoData.js
@@ -0,0 +1,29 @@
+const todoData = [
+  {
+   id: 1,
+   text: "Sort out laundry",
+   isComplete: true
+  },
+  {
+   id: 2,
+   text: "Do some grocery shopping",
+   isComplete: false
+  },
+  {
+   id: 3,
+   text: "Code",
+   isComplete: true
+  },
+  {
+   id: 4,
+   text: "Read a book",
+   isComplete: true
+  },
+  {
+   id: 5,
+   text: "Watch a Netflix movie",
+   isComplete: false
+  }
+]
+
+export default todoData;
