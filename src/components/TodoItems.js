6a1fe78e2563cfde8d7ded27c2dbc8277eb39748
sheygs13 <segun.ekoh@gmail.com>
2020-04-12 16:35:34 +0100

    clean up todoItems file

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import TodoItem from './TodoItem';
-
+import todoData from './todoData';
 
 function TodoItems(){
+ const todos = todoData.map(todo => <TodoItem key={todo.id} list={todo}/>);  
  return (
     <div className="todo-items">
-           <TodoItem/>
-           <TodoItem/>
-           <TodoItem/>
-           <TodoItem/>
+           { todos }
     </div>
  )
 }
