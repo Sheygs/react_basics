d9f16f1c2b177bc27a58202b120d11e2af365ce2
sheygs13 <segun.ekoh@gmail.com>
2020-04-15 14:49:22 +0100

    convert TodoItems to class-based component so that todoData lives in the state

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 import todoData from './todoData';
 
-function TodoItems(){
- const todos = todoData.map(todo => <TodoItem key={todo.id} todo={todo}/>);  
- return (
-    <div className="todo-items">
-           { todos }
-    </div>
- )
+class TodoItems extends Component {
+   constructor(){
+      super();
+      this.state = {
+         items: todoData
+      }
+   }  
+  render(){
+    const { items } = this.state;
+    const todos = items.map(todo => <TodoItem key={todo.id} todo={todo}/>)
+    return (
+       <div className="todo-items">
+              { todos }
+       </div>
+    )
+ }
 }
 
 export default TodoItems;
