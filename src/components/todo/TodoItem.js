2c6a26c8b1d22c7b6b4d62a43dc8456a542a519b
sheygs13 <segun.ekoh@gmail.com>
2020-04-24 12:57:37 +0100

    :recycle:

---
+++ src/components/todo/TodoItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function TodoItem(props){
+ // console.log(props);
+ 
+ const { todo, handleOnChange } = props;
+ const { id, text, isComplete } = todo;
+
+ const styles = {
+   textDecoration: "line-through",
+   color: "grey",
+   fontStyle: "italic"
+ }
+
+ return (
+    <div className="todo-item"> 
+         <input 
+           onChange={ () => handleOnChange(id) } 
+           type="checkbox" 
+           checked={isComplete}
+         />
+         <p style={isComplete ? styles : null }>{text}</p>
+    </div>
+ )
+};
+
+export default TodoItem;
---
+++ src/components/todo/TodoItems.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+import todoData from './todoData';
+
+class TodoItems extends Component {
+   constructor(){
+      super();
+      this.state = {
+         items: todoData
+      }
+      this.handleOnChange = this.handleOnChange.bind(this);
+   }  
+
+  handleOnChange(id){
+      this.setState(previousState => {
+         const updatedItems = previousState.items.map( item => item.id === id ? item.isComplete = !item.isComplete : item);
+         return updatedItems;
+      })
+  }
+
+  render(){
+    const { items } = this.state;
+    const todos = items.map(todo => 
+      <TodoItem 
+        handleOnChange={this.handleOnChange} 
+        key={todo.id} 
+        todo={todo}
+      />
+    );
+    return (
+       <div className="todo-items">
+              { todos }
+       </div>
+    );
+ }
+}
+
+export default TodoItems;
